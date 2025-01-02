336638612ea85824ef96d23a877c7d17c78e0f2c
Zenchyi <107559945+Zenchyi1@users.noreply.github.com>
2025-01-02 17:33:02 +0100

    bumped gns3 version to 2.2.52

--- pkgs/applications/networking/gns3/default.nix
+++ pkgs/applications/networking/gns3/default.nix
@@ -12,26 +12,26 @@ in
 {
   guiStable = mkGui {
     channel = "stable";
-    version = "2.2.51";
-    hash = "sha256-HXuhaJEcr33qYm2v/wFqnO7Ba4lyZgSzvh6dkNZX9XI=";
+    version = "2.2.52";
+    hash = "sha256-/aa3y5c0yPu5GDmPkjN4qWXd2jQiv90hDqrXJTdND68=";
   };
 
   guiPreview = mkGui {
     channel = "stable";
-    version = "2.2.51";
-    hash = "sha256-HXuhaJEcr33qYm2v/wFqnO7Ba4lyZgSzvh6dkNZX9XI=";
+    version = "2.2.52";
+    hash = "sha256-/aa3y5c0yPu5GDmPkjN4qWXd2jQiv90hDqrXJTdND68=";
   };
 
   serverStable = mkServer {
     channel = "stable";
-    version = "2.2.51";
-    hash = "sha256-Yw6RvHZzVU2wWXVxvuIu7GLFyqjakwqJ0EV6H0ZdVcQ=";
+    version = "2.2.52";
+    hash = "sha256-Kd7nTnXKX68Pwg+KwlwMEtQgkpsIgphylZ2D4jjrNgU=";
   };
 
   serverPreview = mkServer {
     channel = "stable";
-    version = "2.2.51";
-    hash = "sha256-Yw6RvHZzVU2wWXVxvuIu7GLFyqjakwqJ0EV6H0ZdVcQ=";
+    version = "2.2.52";
+    hash = "sha256-Kd7nTnXKX68Pwg+KwlwMEtQgkpsIgphylZ2D4jjrNgU=";
   };
 }
 
